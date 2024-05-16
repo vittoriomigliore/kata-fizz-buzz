26628b6f013193986e6b67fc34fd68bc30ec879f
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-05-16 21:40:30 +0200

    lesson-2: test 15 implementation

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,10 +1,14 @@
 class FizzBuzz {
   handle(value: number): string {
+    var fizzBuzz: string = '';
     if((value%3) == 0){
-        return 'Fizz';
+        fizzBuzz += 'Fizz';
     }
     if((value%5) == 0){
-        return 'Buzz';
+        fizzBuzz += 'Buzz';
+    }
+    if(fizzBuzz.length > 0){
+        return fizzBuzz
     }
     return String(value);
   }
