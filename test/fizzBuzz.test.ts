72f1bf4d3ef53494a47a35d5da417e57f4161ffe
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-05-16 22:09:01 +0200

    lesson-2: fix previous test scenarios

--- test/fizzBuzz.test.ts
+++ test/fizzBuzz.test.ts
@@ -23,7 +23,7 @@ describe('fizz buzz test set', function (): void {
 
     var result: string = FizzBuzzClass.handle(5);
 
-    var expected = 'Buzz';
+    var expected = 'BuzzBuzz';
     expect(result).toBe(expected);
   });
 
@@ -31,7 +31,7 @@ describe('fizz buzz test set', function (): void {
 
     var result: string = FizzBuzzClass.handle(15);
 
-    var expected = 'FizzBuzz';
+    var expected = 'FizzBuzzBuzz';
     expect(result).toBe(expected);
   });
 
