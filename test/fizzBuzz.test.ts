78d3eb181273c2241de2782e4b62864a50f5de27
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-05-16 21:29:04 +0200

    lesson-2: fix tests list var

--- test/fizzBuzz.test.ts
+++ test/fizzBuzz.test.ts
@@ -5,7 +5,7 @@ describe('fizz buzz test set', function (): void {
 
   it('test 1', (): void => {
 
-    var result: boolean = FizzBuzzClass.handle(1);
+    var result: string = FizzBuzzClass.handle(1);
 
     var expected = '1';
     expect(result).toBe(expected);
@@ -13,7 +13,7 @@ describe('fizz buzz test set', function (): void {
 
   it('test 3', (): void => {
 
-    var result: boolean = FizzBuzzClass.handle(3);
+    var result: string = FizzBuzzClass.handle(3);
 
     var expected = 'Fizz';
     expect(result).toBe(expected);
@@ -21,7 +21,7 @@ describe('fizz buzz test set', function (): void {
 
   it('test 5', (): void => {
 
-    var result: boolean = FizzBuzzClass.handle(5);
+    var result: string = FizzBuzzClass.handle(5);
 
     var expected = 'Buzz';
     expect(result).toBe(expected);
@@ -29,7 +29,7 @@ describe('fizz buzz test set', function (): void {
 
   it('test 15', (): void => {
 
-    var result: boolean = FizzBuzzClass.handle(15);
+    var result: string = FizzBuzzClass.handle(15);
 
     var expected = 'FizzBuzz';
     expect(result).toBe(expected);
