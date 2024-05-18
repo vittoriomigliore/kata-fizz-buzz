f915a2a94987e80da369420f9e3ebb4c8bbc1048
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-05-18 06:25:53 +0200

    lesson-3: test scenarios list

--- test/fizzBuzz.test.ts
+++ test/fizzBuzz.test.ts
@@ -1,13 +1,37 @@
 import FizzBuzz from "../src/fizzBuzz";
 const FizzBuzzClass = new FizzBuzz();
 
-describe('sample test set', function (): void {
+describe('fizz buzz test set', function (): void {
 
-  it('sample test', (): void => {
+  it('test 1', (): void => {
 
-    var result: boolean = FizzBuzzClass.handle();
+    var result: string = FizzBuzzClass.handle(1);
 
-    var expected = true;
+    var expected = '1';
+    expect(result).toBe(expected);
+  });
+
+  it('test 3', (): void => {
+
+    var result: string = FizzBuzzClass.handle(3);
+
+    var expected = 'Fizz';
+    expect(result).toBe(expected);
+  });
+
+  it('test 5', (): void => {
+
+    var result: string = FizzBuzzClass.handle(5);
+
+    var expected = 'BuzzBuzz';
+    expect(result).toBe(expected);
+  });
+
+  it('test 15', (): void => {
+
+    var result: string = FizzBuzzClass.handle(15);
+
+    var expected = 'FizzBuzzBuzz';
     expect(result).toBe(expected);
   });
 
