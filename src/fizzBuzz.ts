fdc44aee65f5e9b532433b62c5d454bf5519d395
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-05-18 07:21:59 +0200

    lesson-3: implementation by "Obvious Implementation" and TPP

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,24 +1,21 @@
 class FizzBuzz {
   private readonly dictionary: { [key: number]: string } = {
     0: "Fizz",
-    1: "Buzz",
-    2: "FizzBuzz",
+    1: "Buzz"
   };
 
   handle(value: number): string {
-    var result = "1";
-    var index =
-      this.isDividableBy3(value) && this.isDividableBy5(value)
-        ? 2
-        : this.isDividableBy5(value)
-        ? 1
-        : this.isDividableBy3(value)
-        ? 0
-        : -1;
-    if (index != -1) {
-      result = this.dictionary[index];
+    var result = "";
+    if (this.isDividableBy3(value)) {
+      result += this.dictionary[0];
     }
-    return result;
+    if (this.isDividableBy5(value)) {
+      result += this.dictionary[1];
+    }
+    if (result.length > 0) {
+      return result;
+    }
+    return "1";
   }
 
   private isDividableBy5(value: number) {
